2487b1db6dc171b5ba0bbe1b476d6b0732bba12f
Diana Lemen <diana.lemen@gmail.com>
2020-11-10 17:35:20 +0200

    first part OOP & OOD (#1)
    * - first part OOP & OOD
    
    * - fixed path
    
    * - added test
    
    * - fixed regarding the review
    
    * - fixed due to comments

--- src/Point.ts
+++ src/Point.ts
@@ -1,1 +1,42 @@
-export class Point {}
+export class Point {
+  x: number;
+  y: number;
+
+  constructor();
+  constructor(x: number, y: number); 
+  constructor(x = 0, y = 0) {
+    this.x = x;
+    this.y = y;
+  }
+
+  toString(): string {
+    return `(${this.x}, ${this.y})`
+  }
+
+  distance();
+  distance(x: number | Point, y: number);
+  distance(other: Point);
+  distance(x?, y?) {
+    if (x === undefined && y === undefined) {
+      return Math.sqrt(
+        (this.x * this.x) + ((this.y * this.y))
+      )
+    }
+
+    let inputX = x;
+    let inputY = y;
+
+    if (x instanceof Point) {
+      const coordinates = x.toString().replace(/\(/, '').replace(/\)/, '');
+      inputX = coordinates.split(',')[0];
+      inputY = coordinates.split(',')[1];
+    }
+
+    const diffX = this.x - inputX;
+    const diffY = this.y - inputY;
+  
+    return Math.round(Number(Math.sqrt(
+      (diffX * diffX) + (diffY * diffY)
+    ).toFixed(1)))
+  }
+}
--- src/Triangle.ts
+++ src/Triangle.ts
@@ -1,4 +1,39 @@
 import { Shape } from './Shape';
 import { Point } from "./Point";
 
-export class Triangle extends Shape {}
+export class Triangle extends Shape {  
+    constructor(fPoint: Point, sPoint: Point, tPoint: Point);
+    constructor(fPoint: Point, sPoint: Point, tPoint: Point, color: string, filled: boolean);
+    constructor(fPoint, sPoint, tPoint, color?, filled?) {
+      super([fPoint, sPoint, tPoint], color, filled);
+    }
+  
+    toString(): string {
+      const [fPoint, sPoint, tPoint] = this.points;
+      return (
+        `Triangle[v1=(${fPoint.x, fPoint.y}),v2=(${sPoint.x, sPoint.y}),v3=(${tPoint.x, tPoint.y})]`
+      );
+    }
+  
+    getType(): string {
+      const sides: number[] = this.points.map((current, i, arr) => {
+        if (i === arr.length - 1) {
+          return current.distance(arr[0].x, arr[0].y)
+        }
+  
+        return current.distance(arr[i + 1].x, arr[i + 1].y);
+      })
+
+      const filteredSides: number = new Set(sides).size;
+
+      if (filteredSides === 1) {
+        return 'equilateral triangle';
+      }
+
+      if (filteredSides === 2) {
+        return 'isosceles triangle';
+      }
+
+      return 'scalene triangle';
+    }
+}
