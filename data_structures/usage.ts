2c0c75a7d32ca09a79a10e7861a87435bb5b8225
dianalemen <diana.lemen@gmail.com>
2020-12-18 13:28:37 +0200

    Deleted needlees files

--- data_structures/usage.ts
+++ data_structures/usage.ts
@@ -1,2 +1,1 @@
-// to run the priority_q - 1) tsc priority_q.ts 2) node priority_q.js
 // to run the priority_q_class_implementation - 1) tsc priority_q.ts 2) node priority_q_class_implementation.js
